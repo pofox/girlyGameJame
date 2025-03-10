7d13e2ce613d2678510b2410ebbd21a020e008cd
pofox <66141963+pofox@users.noreply.github.com>
2025-03-10 19:06:38 +0200

    bunny animation

--- Assets/scripts/movment.cs
+++ Assets/scripts/movment.cs
@@ -6,6 +6,7 @@ public class movment : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     
     // Reference to the Rigidbody2D component
+    private Animator animator;
     private Rigidbody2D rb;
     private Vector2 movement;
 
@@ -13,6 +14,7 @@ public class movment : MonoBehaviour
     {
         // Get the Rigidbody2D component attached to this object
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,6 +22,18 @@ public class movment : MonoBehaviour
         // Get input from horizontal (A/D or Left/Right arrows) and vertical (W/S or Up/Down arrows) axes
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        if (movement.x != 0)
+        {
+            animator.SetBool("right", true);
+            transform.localScale = new Vector3(Mathf.Sign(movement.x) * Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+        else animator.SetBool("right", false);
+
+        if (movement.y > 0) animator.SetBool("up", true);
+        else animator.SetBool("up", false);
+        
+        if (movement.y < 0) animator.SetBool("down", true);
+        else animator.SetBool("down", false);
     }
 
     void FixedUpdate()
